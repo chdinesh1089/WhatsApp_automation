eaecdb847ee34ae92887ab12f17fafdf3b32b489
Dinesh <chdinesh1089@gmail.com>
2020-12-03 22:15:15 +0530

    Update xpaths and minor changes

--- main.py
+++ main.py
@@ -4,36 +4,33 @@ from selenium.webdriver.common.keys import Keys
 import getpass
 import time
 
-#xpaths
-menu= "//*[@class='_3j8Pd'][2]"
+# xpaths
+menu= "//*[@title='Menu']"
 new_grp = "//*[text()='New group']"
 contact_field = "//*[@placeholder='Type contact name']"
-create_grp =  "//*[@data-icon='forward-light']"
+create_grp =  "//*[@data-icon='arrow-forward']"
 msg_box = "//*[text()='Type a message']"
 
-#contact names should be exact
-contact_names = ['Chaitanya','Nishanth']
-group_name = 'name'
-msg_txt = "just Some Automations"
+# contact names should be exact
+contact_names = ['Chaitanya','Sai Kumar']
+group_name = 'CT Automation Test'
+msg_txt = "Just Some cool automations"
 
 driver = webdriver.Chrome()
 grp_name_actions= ActionChains(driver)
 msg_actions = ActionChains(driver)
 driver.get('https://web.whatsapp.com/')
-time.sleep(15)
+
+## Scan QR in this time.
+time.sleep(20)
 
 (driver.find_element_by_xpath(menu)).click()
 (driver.find_element_by_xpath(new_grp)).click()
 
-#adding contacts to group
+# adding contacts to group
 contact_fld_ele = driver.find_element_by_xpath(contact_field)
 for cntct in contact_names:
     time.sleep(0.5)
-    #contact_fld_ele.click()
-    #actions.send_keys(cntct)
-    #actions.perform()
-    #actions.send_keys("u'\ue007'")
-    #actions.perform()
     time.sleep(1)
     contact_fld_ele.send_keys(cntct)
     contact_fld_ele.send_keys(Keys.RETURN)
@@ -47,9 +44,7 @@ grp_name_actions.send_keys(group_name)
 grp_name_actions.send_keys(Keys.RETURN)
 grp_name_actions.perform()
 time.sleep(3)
-'''msg_ele= driver.find_element_by_xpath(msg_box)
-msg_ele.send_keys("hello")
-msg_ele.send_keys(Keys.RETURN)'''
+
 #sending message
 msg_actions.send_keys(msg_txt)
 msg_actions.send_keys(Keys.RETURN)
